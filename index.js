c411c2932a14a3eaa9f8e86debacbeb17bccdc97
Si Si <96635252+sssssissssss@users.noreply.github.com>
2024-03-02 00:25:50 +0200

    refactor

--- index.js
+++ index.js
@@ -1,43 +1,74 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ObjectId } = require('mongodb');
-require('dotenv').config();
-const url = process.env.URL;
-
 app.use(express.json());
 
-const client = new MongoClient(url);
-let db;
+const { MongoClient, ObjectId } = require('mongodb');
+require('dotenv').config();
+const client = new MongoClient(process.env.URI);
+
+let dataBase = null;
+
+async function run() {
+    try {
+        await client.connect();
+        dataBase = client.db('test');
+        console.log('Connected to the database');
+    } catch (error) {
+        console.error('Failed to connect:', error);
+    }
+}
 
 
-client.connect().then(() => db = client.db('Books_Store'));
 
-app.listen(3000, (req, res) => {
-    console.log('doWork');
+app.listen(process.env.PORT || 3000, (req, res) => {
+    console.log('Server is running ');
+    run();
 })
 
-app.get('/books', function (req, res) {
-    let ans = [];
-    db.collection('books').find().forEach(it => ans.push(it)).then(() => res.send(ans))
+app.get('/books', async function (req, res) {
+    const coll = dataBase.collection('books');
+    const cursor = coll.find();
+    const result = [];
+
+    for await (const it of cursor)
+        result.push(it);
+
+    res.send(result);
 })
 
-app.get('/books/:id', function (req, res) {
+app.get('/books/:id', async function (req, res) {
 
     if (ObjectId.isValid(req.params.id)) {
-        console.log(req.params.id);
-        db.collection('books').findOne({ _id: new ObjectId(req.params.id) })
-            .then((doc) => { res.send(doc) })
+        const coll = dataBase.collection('books');
+        let result = await coll.findOne({ _id: new ObjectId(req.params.id) });
+
+        if (result)
+            res.send(result);
+        else
+            res.send({ Error: 'Not Found' });
 
     } else {
+        res.status(401).send({ Error: "Invalid ID" });
     }
 
 })
 
-app.post('/books', (req, res) => {
-    console.log(req.body);
-    db.collection('books').insertOne(req.body).then((doc) => res.send(doc))
+app.post('/books', async (req, res) => {
+    const coll = dataBase.collection('books');
+    const doc = req.body;
+    const result = await coll.insertOne(doc);
+    res.send(result.acknowledged);
 })
 
-app.delete('/books/:id', (req, res) => {
-    db.collection('books').deleteOne({ _id: new ObjectId(req.params.id) }).then((doc) => res.send(doc))
+app.delete('/books/:id', async (req, res) => {
+    if (ObjectId.isValid(req.params.id)) {
+        const coll = dataBase.collection('books');
+        const result = await coll.deleteOne({ _id: new ObjectId(req.params.id) });
+        if (result.deletedCount)
+            res.send(result);
+        else
+            res.send({ Error: 'Not Found' });
+    } else {
+        res.status(401).send({ Error: "Invalid ID" });
+    }
 })
